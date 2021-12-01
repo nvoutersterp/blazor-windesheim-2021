98c5766fb5452fa6c20bb51dc36e8fef95274bde
JP <jp@jp.nl>
2021-12-01 12:32:09 +0100

    Autocompleter a la TDD aan het opbouwen

---
+++ DemoSolution/DemoProject.Tests/AutocompleterTests.cs
@@ -0,0 +1,123 @@
+using DemoProject.Shared;
+using DemoProject.Tests.Models;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DemoProject.Tests
+{
+	public class AutocompleterTests
+	{
+		List<Car> cars;
+		Autocompleter<Car> sut;
+
+		[SetUp]
+		public void Setup()
+		{
+			sut = new Autocompleter<Car>(); // system under test
+			cars = new List<Car>
+			{
+				new Car { Make = "BMW", Model ="e46"},
+				new Car { Make = "Opel", Model ="Astra"},
+				new Car { Make = "Fiat", Model = "Multipla" },
+				new Car { Make = "Mazda", Model = "MX5" },
+				new Car { Make = "Toyota", Model = "MR2" },
+				new Car { Make = "Bugati", Model = "Veyron" },
+				new Car { Make = "Mazda", Model = "RX7" },
+				new Car { Make = "Opel", Model = "Carsa" }
+			};
+		}
+
+		[Test]
+		public void Autocomplete_WithBasicQuery_ShouldMakeSuggestions()
+		{
+			sut.Data = cars;
+			sut.Query = "e";
+
+			sut.Autocomplete();
+
+			var expected = new List<Car>
+			{
+				cars[0],
+				cars[1],
+				cars[5],
+				cars[7],
+			};
+			Assert.AreEqual(expected, sut.Suggestions.Select(x => x.Item).ToList());
+		}
+
+		[Test]
+		public void Autocomplete_WithCapitalQuery_ShouldMakeCaseInsensitiveSuggestions()
+		{
+			sut.Data = cars;
+			sut.Query = "O";
+
+			sut.Autocomplete();
+
+			var expected = new List<Car>
+			{
+				cars[1],
+				cars[4],
+				cars[5],
+				cars[7]
+			};
+			Assert.AreEqual(expected, sut.Suggestions.Select(x => x.Item).ToList());
+		}
+
+		[Test]
+		public void Autocomplete_QueryThatMatchesMultipleProperties_ShouldAddItemsUniquely()
+		{
+			sut.Data = cars;
+			sut.Query = "a";
+
+			sut.Autocomplete();
+
+			var expected = cars.Skip(1).ToList();
+			Assert.AreEqual(expected, sut.Suggestions.Select(x => x.Item).ToList());
+		}
+
+		[Test]
+		public void Next_NothingHighlighted_HighlightFirstItem()
+        {
+			sut.Data = cars;
+			sut.Query = "a";
+
+			sut.Autocomplete();
+			sut.Next();
+
+			Assert.AreEqual(true, sut.Suggestions.First().IsHighlighted);
+			Assert.AreEqual(1, sut.Suggestions.Count(x => x.IsHighlighted));
+		}
+
+		[Test]
+		public void Next_FirstItemHighlighted_HighlightSecondItem()
+		{
+			sut.Data = cars;
+			sut.Query = "a";
+
+			sut.Autocomplete();
+			sut.Next();
+			sut.Next();
+
+			Assert.AreEqual(true, sut.Suggestions[1].IsHighlighted);
+			Assert.AreEqual(1, sut.Suggestions.Count(x => x.IsHighlighted));
+		}
+
+		[Test]
+		public void Next_LastItemHighlighted_HighlightFirstItem()
+		{
+			sut.Data = cars;
+			sut.Query = "a";
+
+			sut.Autocomplete();
+            foreach (var suggestion in sut.Suggestions)
+            {
+				sut.Next();
+            }
+			sut.Next();
+
+			Assert.AreEqual(true, sut.Suggestions.First().IsHighlighted);
+			Assert.AreEqual(1, sut.Suggestions.Count(x => x.IsHighlighted));
+		}
+	}
+}
---
+++ DemoSolution/DemoProject/Shared/Autocompleter.razor.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject.Shared
+{
+	public partial class Autocompleter<T> : ComponentBase
+	{
+
+		public List<T> Data { get; set; }
+
+		public string Query { get; set; }
+
+		public List<AutocompleterItem> Suggestions { get; set; }
+
+		public void Autocomplete()
+		{
+			Suggestions = new List<AutocompleterItem>();
+
+			foreach (var item in Data)
+			{
+				// reflection
+				var props = item.GetType().GetProperties();
+				foreach (var prop in props)
+				{
+					if (prop.PropertyType == typeof(string))
+					{
+						var value = (string)prop.GetValue(item);
+						if (value.ToLower().Contains(Query.ToLower()))
+						{
+							Suggestions.Add(new AutocompleterItem
+							{
+								Item = item
+							});
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		public void Next()
+		{
+			var index = Suggestions.FindIndex(x => x.IsHighlighted);
+			if (index > -1)
+			{
+				Suggestions[index].IsHighlighted = false;
+				Suggestions[(index + 1) % Suggestions.Count].IsHighlighted = true;
+				return;
+			}
+
+			Suggestions.First().IsHighlighted = true;
+		}
+
+		public class AutocompleterItem
+		{
+			public bool IsHighlighted { get; set; }
+
+			public T Item { get; set; }
+		}
+	}
+}
